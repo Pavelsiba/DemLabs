bad85e07e8ef402f2787e7d8e00d8cf40b96a3dd
Pavelsiba <105110809+Pavelsiba@users.noreply.github.com>
2023-03-18 10:45:04 +0700

    final

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,53 +2,53 @@
   <div class="relative flex min-h-screen">
     <!--sidebar-->
     <div class="sidebar">
-      <div class="h-10">
-        <Logo name="logo" class="w-12 h-12" />
+      <div class="h-[62px]">
+        <Cellframe name="cellframe" class="w-36 h-16 pl-4" />
       </div>
-      <nav class="font-nav">
+      <nav class="text-[13px] leading-4">
         <NuxtLink to="/" class="link"
           ><Wallet name="wallet" class="w-6 h-3.5 mr-5"></Wallet>
-          <span class="text-white">Wallet</span>
+          <span>Wallet</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Dex name="dex" class="w-6 h-6 mr-5"></Dex>
-          <span class="text-white">DEX</span>
+          <span>DEX</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Txexplorer name="txexplorer" class="w-6 h-6 mr-5"></Txexplorer>
-          <span class="text-white">TX explorer</span>
+          <span>TX explorer</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Certificates name="certificates" class="w-6 h-6 mr-5"></Certificates>
-          <span class="text-white">Certificates</span>
+          <span>Certificates</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Tokens name="tokens" class="w-6 h-6 mr-5"></Tokens>
-          <span class="text-white">Tokens</span>
+          <span>Tokens</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Vclients name="vclients" class="w-6 h-6 mr-5"></Vclients>
-          <span class="text-white">VPN client</span>
+          <span>VPN client</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Vservice name="vservice" class="w-6 h-6 mr-5"></Vservice>
-          <span class="text-white">VPN service</span>
+          <span>VPN service</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Conslesvg name="conslesvg" class="w-6 h-6 mr-5"></Conslesvg>
-          <span class="text-white">Console</span>
+          <span>Console</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Logs name="logs" class="w-6 h-6 mr-5"></Logs>
-          <span class="text-white">Logs</span>
+          <span>Logs</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Settings name="settings" class="w-6 h-6 mr-5"></Settings>
-          <span class="text-white">Settings</span>
+          <span>Settings</span>
         </NuxtLink>
         <NuxtLink to="/" class="link"
           ><Dapps name="dapps" class="w-6 h-6 mr-5"></Dapps>
-          <span class="text-white">Dapps</span>
+          <span>dApps</span>
         </NuxtLink>
       </nav>
     </div>
@@ -57,7 +57,7 @@
       <!--Header-->
       <div class= "search">Search</div>
       <!--Content-->
-      <div class="p-8 text-cyan-700 font-extrabold">Content</div>
+      <div class="p-8 font-extrabold">Content</div>
     </div>
   </div>
 </template>
@@ -75,6 +75,7 @@ import Conslesvg from "./Icons/Conslesvg.vue";
 import Logs from "./Icons/Logs.vue";
 import Settings from "./Icons/Settings.vue";
 import Dapps from "./Icons/Dapps.vue";
+import Cellframe from "./Icons/Cellframe.vue";
 
 export default {
   components: {
@@ -89,7 +90,8 @@ export default {
     Conslesvg,
     Logs,
     Settings,
-    Dapps
+    Dapps,
+    Cellframe
   },
 };
 </script>
@@ -99,10 +101,11 @@ a:hover {
   box-shadow: 1px 1px 0px rgba(107, 102, 126, 0.49), inset 3px 3px 5px rgba(8, 7, 13, 0.25);
 }
 
-.font-nav {
+/* .font-nav {
   font-family: "Quicksand", sans-serif;
   font-style: normal;
   font-size: 13px;
   line-height: 16px;
-}
+  color: white;
+} */
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ import Sidebar from "../components/Sidebar.vue";
 </script>
 
 <template>
-    <Sidebar />
+  <Sidebar />
 </template>
 
 <style></style>
